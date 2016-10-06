f626402d3e1bd10da3f84187a7b4fc53f8dd7489
Atrusilo1 <atrusilo1@studentmail.nait.ca>
2016-10-06 11:53:16 -0600

    added a new website to the soultion

---
+++ Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eResturant.Startup))]
+namespace eResturant
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
